7aadf6bc8b8d111d4e15072400708528171f91d5
priyankk01 <42462437+priyankk01@users.noreply.github.com>
2019-02-14 21:52:54 +0530

    Add files via upload

---
+++ repeat.py
@@ -0,0 +1,14 @@
+n=int(input())
+l=list(map(int,input().split()))
+p=[]
+f=0
+for i in l:
+    if(i not in p):
+        p.append(i)
+    else:
+        f=1
+        print(i)
+        break
+if(f==0):
+    print("'unique'")
+
