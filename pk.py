a50e7a4e1c6ad6e599525df718a9512bd4a13730
priyankk01 <42462437+priyankk01@users.noreply.github.com>
2019-02-14 19:18:19 +0530

    Add files via upload

---
+++ pk.py
@@ -0,0 +1,19 @@
+n=int(input())
+p=[]
+k=0
+l=list(map(int,input().split()))
+l=sorted(l)
+for i in range(len(l)):
+    c=l.count(l[i])
+    if(c>1):
+        if(l[i] not in p):
+            p.append(l[i])
+        i=i+c-1
+        k=k+1
+if(k==0):
+    print("unique")
+else:
+    for i in p:
+        print(i,end=" ")
+        
+
